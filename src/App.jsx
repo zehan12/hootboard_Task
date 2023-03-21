59cf59544b82b168a8eae0912181dfb8c2d2396d
Zehan Khan <zehan9211@gmail.com>
2023-03-22 01:16:15 +0530

    added images for weather info

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import Form from './pages/Form'
 
 const App = () => (
-  <div className='container flex items-center justify-center h-screen'>
+  <div className='container flex items-center justify-center h-[90vh]'>
     <Form />
   </div>
 )
